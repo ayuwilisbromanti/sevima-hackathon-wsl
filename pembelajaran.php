36baef2232492b79b31602f4240d20fa5f12cc9b
ayuwilisbromanti <87847608+ayuwilisbromanti@users.noreply.github.com>
2023-06-24 13:57:09 +0700

    interface halaman utama

--- pembelajaran.php
+++ pembelajaran.php
@@ -4,7 +4,13 @@ include "navbar.php";
 <!-- sales report area start -->
     <div class="card col-md-12">
         <div class="card-body">
-            This is some text within a card body.
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/qPfgQK8ZsI4?rel=0" allowfullscreen></iframe>
+        </div>
+        <span>Catatan :</span>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
         </div>
     </div>
                     <!-- sales report area end -->
